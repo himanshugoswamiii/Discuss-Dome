9cd97c17350d4b6a94a07b12f32ff9b0f4cd28ac
Himanshu <himanshugoswami.12feb@gmail.com>
2023-08-29 01:15:43 +0530

    Initial Commit

---
+++ base/apps.py
@@ -0,0 +1,7 @@
+# No-changes done in this file : Default contents
+from django.apps import AppConfig
+
+
+class BaseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'base'
